18c3ab585a5dbeeae70ca787499de605b20f641f
FujinoJunpei <fujino.junpei@unipro.co.jp>
2020-06-11 17:25:43 +0900

    remove '\n'

--- src/streaming/streaming.py
+++ src/streaming/streaming.py
@@ -10,14 +10,14 @@ import emoji
 
 def preprocess_text(df):
     text = df["text"]
-    text = text.str.strip() # 余分な空白削除
-    text = text.apply(normalize) # 正規化(半角/全角変換など)
-    text = text.str.lower() # 大文字→小文字
+    # text = text.str.strip() # 余分な空白削除
+    # text = text.apply(normalize) # 正規化(半角/全角変換など)
+    # text = text.str.lower() # 大文字→小文字
     text = text.str.replace("\n", " ") # 改行コード削除
     
     # 絵文字を置換
-    for i in range(len(text)):
-        text[i] = emoji.demojize(text[i], delimiters=("",""))
+    # for i in range(len(text)):
+    #     text[i] = emoji.demojize(text[i], delimiters=("",""))
     
     df["text"] = text
     
@@ -30,7 +30,7 @@ def input():
     #     j.write(target_jsonl)
     if target_jsonl is None:
         return 0
-    # target_jsonl = preprocess_text(target_jsonl)
+    target_jsonl = preprocess_text(target_jsonl)
     with open(os.path.join(os.path.dirname(__file__),'../../data/input.jsonl'), "w") as f:
         for _, row in target_jsonl.iterrows():
             f.write(f'{{"id":{int(row[0])},"text":"{row[1]}"}}\n')
