d34efcbfd5182626f17d42a5310f6d83d4dc5c0a
FujinoJunpei <fujino.junpei@unipro.co.jp>
2020-06-12 10:15:50 +0900

    add input_sample, output_sample

--- spreadsheet_write.py
+++
@@ -1,6 +0,0 @@
-s = ''
-for i in range(1000):
-    s += '=IMPORTRANGE("1g2Y4c2JnCA7IPKyUiSNASSKwfs9LlxsiRStKxoPImFU", "Form Responses 1!B'+ str(i+2) + '")\n'
-
-with open("./spreadsheet_write.txt", mode='w') as f:
-    f.write(s)
